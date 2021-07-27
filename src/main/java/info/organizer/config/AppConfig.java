73ac3a6243676caa3470fdeaabad6090519efac4
Anastacia Timohova <nanakoalabanana@gmail.com>
2021-07-27 21:59:57 +0300

    'initial'

---
+++ src/main/java/info/organizer/config/AppConfig.java
@@ -0,0 +1,13 @@
+package info.organizer.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"info.organizer"})
+public class AppConfig implements WebMvcConfigurer {
+
+}
---
+++ src/main/java/info/organizer/service/UserService.java
@@ -0,0 +1,9 @@
+package info.organizer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+}
